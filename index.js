93ba50235e24cc8556db242f1a24cfda0d8ba8fe
Ran Pol <rnd809@gmail.com>
2022-09-18 01:59:32 -0400

    HTTP Request Body: The different type of data that a client can send to the server. The Express server can use middleaware to determine how to digest the information.

--- index.js
+++ index.js
@@ -2,26 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.text())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.post("/user", (req, res) => {
+    console.log(req.body)
   res.send("Hello worl!");
 });
 
-app.get("/myfiles", (req, res) => {
-  res.sendFile("./javascript.png", {
-    root: __dirname,
-  });
-});
-
-app.get("/user", (req, res) => {
-  res.json({
-    name: "john",
-    lastname: "john",
-    adress : "john",
-  });
-});
-
-app.get("/isAlive", (req, res) => {
-    res.sendStatu(204);
-  });
 app.listen(3000);
 console.log(`Server on port ${3000}`);
