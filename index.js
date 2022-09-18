82a5f936baa68ded17590b2aa68df5817b5f45ef
Ran Pol <rnd809@gmail.com>
2022-09-18 02:51:33 -0400

    Request Params: Extracting data from the url petition made my the client with the following syntax => req.params

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.text())
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-
-app.post("/user", (req, res) => {
-    console.log(req.body)
-  res.send("Hello worl!");
+app.get("/hello/:username", (req, res) => {
+  res.send(`Hello ${req.params.username}`);
 });
+
+app.get("/add/:x/:y", (req, res) => {
+  const { x, y } = req.params;
+  res.send(`The total sum: ${parseInt(x) + parseInt(y)}`);
+});
+
+app.get("/users/:username/photo", (req, res) => {
+  console.log(req.params);
+  const { username } = req.params;
+  if (username === "car") {
+    return res.sendFile("./javascript.png", { root: __dirname });
+  }
+  res.send("The user don't hace access!");
+});
+
+app.get("/name/:name/age/:age", (req, res) => {
+    const xParams= req.params;
+    res.send('Hello');
+    console.log(xParams)
+  });
 
 app.listen(3000);
 console.log(`Server on port ${3000}`);
