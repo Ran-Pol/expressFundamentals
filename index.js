a5a696b5307c3cb66b2b04ee6a95d45e25562498
Ran Pol <rnd809@gmail.com>
2022-09-18 04:11:07 -0400

    Third Party Middlewares: npm instal morgan as an example to see outside middleswares that can be install in the project.

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const express = require("express");
-const { nextTick } = require("process");
+const morgan = require("morgan");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Pass throught here!");
-  next();
-});
 
-app.use((req, res, next) => {
-  if(req.query.login==="jordan"){
-      next()
-  }else{
-      res.send("Not authorized page!")
-  }
-});
+app.use(morgan('tiny'))
+
 
 app.get("/profile", (req, res) => {
   res.send("profile page");
@@ -24,9 +15,18 @@ app.get("/about", (req, res) => {
   res.send("about page");
 });
 
+app.use((req, res, next) => {
+    if(req.query.login==="jordan"){
+        next()
+    }else{
+        res.send("Not authorized page!")
+    }
+  });
+  
+
 app.get("/dashboard", (req,res)=>{
     res.send("Inside the dashboard!")
 })
 
 app.listen(3000);
-console.log(`Server on port ${3000}`);
+console.log(`Server  on port ${3000}`);
