e05f05aac24fd76674320e1d19ac2e9884fadc8f
Ran Pol <rnd809@gmail.com>
2022-09-17 21:44:37 -0400

    Rounting: Express allows us to better handle the client request and prevents as using the if/else to handle the differents clients routes request.

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// const http = require("http");
-// const fs = require("fs");
-
-// const server = http.createServer((req, res) => {
-//   const read = fs.createReadStream("./static/index.html");
-//   read.pipe(res);
-// });
-
-// server.listen(3000);
-// console.log(`Server on port ${3000}`);
-
 const express = require("express");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile("./static/index.html", {
-    root: __dirname,
-  });
+    res.send('Hello world')
 });
+
+app.get("/about", (req, res) => {
+    res.send('About')
+});
+
+app.get("/weather", (req, res) => {
+    res.send('The current weather is nice')
+});
+
+app.use((req, res) => {
+    res.status(404).send('Didn\'t Find That Request')
+});
+
 
 app.listen(3000);
 console.log(`Server on port ${3000}`);
