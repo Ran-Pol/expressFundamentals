c34b8e26057eb4a5e53458e5755516380f0f337b
Ran Pol <rnd809@gmail.com>
2022-09-18 20:47:50 -0400

    Static Files configuration in express

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 
@@ -12,6 +13,14 @@ app.set("case sensitive routing", true);
 app.use(express.json());
 app.use(morgan("tiny"));
 
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.get("/note.txt", (req, res) => {
+  res.send("This is not a file");
+});
+
 app.get("/profile", (req, res) => {
   res.send("profile page");
 });
@@ -20,17 +29,19 @@ app.get("/about", (req, res) => {
   res.send("about page");
 });
 
-app.use((req, res, next) => {
-  if (req.query.login === "jordan") {
-    next();
-  } else {
-    res.send("Not authorized page!");
-  }
-});
+// app.use((req, res, next) => {
+//   if (req.query.login === "jordan") {
+//     next();
+//   } else {
+//     res.send("Not authorized page!");
+//   }
+// });
 
 app.get("/dashboard", (req, res) => {
   res.send("Inside the dashboard!");
 });
+
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.listen(app.get("port"));
 console.log(`Server ${app.get("appName")} on port ${app.get("port")}`);
