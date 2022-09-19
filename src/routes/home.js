b1792e35e21b70a23d95243956cfdbaf16ab258e
Ran Pol <rnd809@gmail.com>
2022-09-19 02:22:56 -0400

    Making Async Calls to another server using the axios module

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const axios = require("axios");
 
 const router = Router();
 
@@ -26,6 +27,12 @@ router.get("/about", (req, res) => {
 
 router.get("/dashboard", (req, res) => {
   res.render("dashboard");
+});
+router.get("/post", async (req, res) => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  res.render("post", { posts: response.data });
 });
 
 module.exports = router;
